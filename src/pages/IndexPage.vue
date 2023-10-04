bbdf9ff56cd3b33b6cda4049e2e48c381ec74bbf
saknarin818 <saknarin828@gmail.com>
2023-10-04 13:06:09 +0700

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,34 @@
-# IndexPage.vue
 <template>
   <div class="htmlClass">
     <h1>{{ title }}</h1>
+    
   </div>
+  <InputPage></InputPage>
+  <FunctionPage></FunctionPage>
 </template>
 
 
 <script>
 export default {
-  name: 'HtmlComponent',
-  data() {
-    return {
-      title: 'Vue is awesome.',
-    }
-  },
+    name: 'HtmlComponent',
+    component: {
+        InputPage
+    },
+    component: {
+      FunctionPage
+    },
+
+    data() {
+        return {
+            title: '390 ศักดิ์นรินทร์ ม่วงศิริ',
+        };
+    },
+    components: { InputPage,FunctionPage },
+
 }
+import FunctionPage from 'src/pages/FunctionPage.vue';
+import InputPage from 'src/pages/InputPage.vue';
+
 </script>
 
 <style scoped></style>
